f6231a9c360fc2cd544feca77d093062c4584d48
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-16 11:46:48 +0000

    basic math in repr

---
+++ add.js
@@ -0,0 +1,13 @@
+const add = (a, b) => {
+    return a + b
+};
+
+// console.log(add(2, 2));
+
+module.exports = add;
+
+/////// for REPR
+// const add = require('./add');
+// console.log(add(2,2));
+
+// console.log(2 * add(4,4));
---
+++ multiply.js
@@ -0,0 +1,11 @@
+const multiply = (a, b) => {
+    return a * b
+};
+
+// console.log(multiply(2, 2));
+
+module.exports = multiply;
+
+/////// for REPR
+// const multiply = require('./multiply');
+// console.log(multiply(2,2));
