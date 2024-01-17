8f412b926254479ce50919ad8419ab5b20b997fb
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-17 10:24:45 +0000

    arrays

--- arrays.js
+++ arrays.js
@@ -21,5 +21,25 @@
 
 /////// Challenge
 const addToBatch = (array, number) => {
-    
+    if (array.length < 5) {
+        const newArray = array.concat(number);
+        return newArray
+    } else {
+        return array
+    }
 }
+
+addToBatch([1], 3); 
+// [ 1, 3 ]
+
+addToBatch([1, 2, 3], 4); 
+// [ 1, 2, 3, 4 ]
+
+addToBatch([], 8); 
+// [ 8 ]
+
+addToBatch([1, 2, 3, 4, 5, 6], 7); 
+// [ 1, 2, 3, 4, 5, 6 ]
+
+addToBatch([1, 2, 3, 4, 5, 6, 7, 8, 9], 10); 
+// [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
